269b88f6cddfb78d39d2043401aee3b128922f58
hyzm <990473625@qq.com>
2023-03-13 00:52:19 +0800

    a

--- src/configuration.rs
+++ src/configuration.rs
@@ -33,7 +33,9 @@ pub struct ApplicationSettings {
 #[derive(serde::Deserialize)]
 pub struct EmailClientSettings {
     pub base_url: String,
-    pub sender_emial: String,
+    pub sender_email: String,
+    pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
@@ -111,6 +113,10 @@ impl TryFrom<String> for Environment {
 
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
-        SubscriberEmail::parse(self.sender_emial.clone())
+        SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,45 @@
 use std::net;
+use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use tracing_actix_web::TracingLogger;
 
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    let timeout = configuration.email_client.timeout();
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool, email_client)
+}
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
 pub fn run(
-    listener: net::TcpListener,
+    listener: TcpListener,
     db_pool: Pool<Postgres>,
     email_client: EmailClient,
 ) -> Result<Server, std::io::Error> {
